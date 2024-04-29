c47c5c0a8eb9724db8e20d0d7251c85311cdef11
Fabio Tondin <fabiotperez@gmail.com>
2024-04-29 19:58:26 -0300

    chore: added url and method http in error message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Mitm = require('mitm');
 const mitm = Mitm();
 
 mitm.on('request', (req, res) => {
-  throw new Error('Network requests forbidden in offline mode');
+  throw new Error(`Network requests forbidden in offline mode: ${req.method} ${req.url}`);
   res.end();
 });
